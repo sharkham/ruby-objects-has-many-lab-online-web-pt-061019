9cb079ae2547861eef5e8afc6c59d001e6aa7455
Sam Markham <sam.e.markham@gmail.com>
2019-07-12 20:06:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
   attr_accessor :name
   attr_reader :posts
+  ALL_POSTS = []
   def initialize(name)
     @name = name
     @posts = []
@@ -8,13 +9,18 @@ class Author
 
   def add_post(post)
     @posts << post
+    ALL_POSTS << post
     post.author = self
   end
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
     @posts << post
+    ALL_POSTS << post
     post.author = self
   end
 
+  def self.post_count
+    ALL_POSTS 
+  end
 end
