5565882335459cc9435a1691b0ec9ed48eb7054a
Sam Markham <sam.e.markham@gmail.com>
2019-07-12 19:58:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,10 @@ class Artist
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
-    song.artist = self 
+    song.artist = self
+  end
+
+  def song_count
+    @songs.count 
   end
 end
